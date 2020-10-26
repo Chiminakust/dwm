9879ad6973eb3d0a7e507aafb17ba3a2bbc407f2
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-10-26 10:56:27 -0400

    added gitignore and shiftview patch

--- dwm.c
+++ dwm.c
@@ -2127,6 +2127,29 @@ zoom(const Arg *arg)
 	pop(c);
 }
 
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+void
+shiftview(const Arg *arg)
+{
+	Arg shifted;
+
+	/* left circular shift */
+	if(arg->i > 0)
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+	/* right circular shift */
+	else
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	view(&shifted);
+}
+
 int
 main(int argc, char *argv[])
 {
