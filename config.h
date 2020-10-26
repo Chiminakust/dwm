43caea12eddcf4d1f1f7192c3b93e3c93c388df8
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-10-26 11:25:02 -0400

    added toggle fullscreen (actualfullscreen) patch

--- config.h
+++ config.h
@@ -91,6 +91,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY | ShiftMask,           XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY | ShiftMask,           XK_equal,  setgaps,        {.i = GAP_TOGGLE } },
+	{ MODKEY | ShiftMask,           XK_f,      togglefullscr,  {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
