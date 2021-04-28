390a3038dcfad1a7e6efb04cc18fee55231a659e
Maxime Guillemette <m.guillemette@softdb.com>
2021-04-27 22:02:52 -0400

    made teams, discord, skype and pavucontrol all spawn on tag 9

--- config.h
+++ config.h
@@ -40,8 +40,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class             instance           title       tags mask     isfloating   monitor */
+	{ "Gimp",            NULL,              NULL,       0,            1,           -1 },
+	{ "discord",         "discord",         "Discord",  1 << 8,       0,           -1 },
+	{ "Skype",           "skype",           "Skype",    1 << 8,       0,           -1 },
+	{ "Microsoft Teams", "microsoft teams", NULL,       1 << 8,       0,           -1 },
+	{ "Pavucontrol",     "pavucontrol",     NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
